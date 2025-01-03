18913b3be13a8ab68c3a11c96076918a3255be86
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-03 14:03:39 +0700

    Add 4.basic_operations.py

---
+++ 2.Conditional_Statements/4.basic_operations.py
@@ -0,0 +1,9 @@
+a = float(input())
+b = float(input())
+print("Tong:", a + b)
+print("Hieu:", a - b)
+print("Tich:", a * b)
+if b != 0:
+    print("Thuong:", a / b)
+else:
+    print("Khong chia duoc cho 0")
